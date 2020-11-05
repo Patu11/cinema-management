af2a4fe120897f480fd764839d428dd35186c836
Patryk <patry07@o2.pl>
2020-11-05 01:05:42 +0100

    splitting project into client and server packages

--- src/server/App.java
+++ src/server/App.java
@@ -1,4 +1,4 @@
-package sample;
+package server;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
--- src/server/Movie.java
+++ src/server/Movie.java
@@ -1,4 +1,4 @@
-package sample;
+package server;
 
 import java.util.Date;
 
