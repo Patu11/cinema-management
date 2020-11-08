4a02e3ce31001ef4c360f28844a1a356bce69bcf
Patryk <patry07@o2.pl>
2020-11-08 23:56:00 +0100

    cleaned up some files

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,16 +1,15 @@
 package server;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class ServerMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
         primaryStage.setTitle("Server");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
